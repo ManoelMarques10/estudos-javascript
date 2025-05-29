342fbc73403352b618a39c02ad54e41e4a7caa06
Manoel Marques <Manorlgames@Outlook.com>
2025-05-29 09:28:03 -0300

    .
    .

---
+++ JavaScript/Aula 11/Exx008.js
@@ -0,0 +1,8 @@
+var vel = 50.5
+console.log (`a velocidade do seu carro é ${vel}km/h.`)
+if (vel > 60) {
+    console.log ('você foi multado por estar acima da velocidade permitida')
+} 
+else {
+    console.log ('está tudo ok')
+}
