30b38a40afad8215ea3ee5db05c4af5cea705cc2
Manoel Marques <Manorlgames@Outlook.com>
2025-06-03 13:29:51 -0300

    Estruturas de repetição.
    Estruturas de repetição.

---
+++ JavaScript/Aula 14/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+while (c <= 5) {
+    console.log(`Oi ${c}`)
+    c += 1
+}
+
+console.log('Vai começar...')
+for (var c = 1; c <=5; c++) {
+    console.log (`Olá ${c}`)
+}
+console.log('Fim.')
