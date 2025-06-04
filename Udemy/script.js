ca66b2aa2fc564766184eb2eadf4f2c8b867091d
Manoel Marques <Manorlgames@Outlook.com>
2025-06-04 11:27:49 -0300

    Curso Udemy
    Curso Udemy

---
+++ Udemy/script.js
@@ -0,0 +1,9 @@
+var nome = prompt("Qual é seu nome?");
+
+var sobrenome = prompt("Qual é seu sobrenome?")
+
+console.log("Olá, " + nome + " " + sobrenome + "!");
+
+document.writeln("<h1> Bem vindo ao projeto "+ nome + " " + sobrenome + ".</h1>");
+
+document.writeln("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve.' />")
